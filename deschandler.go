5bac754db21922f995fd70bf0d5de281bd4acb3f
HenryLee <henrylee_cn@foxmail.com>
2016-04-09 15:11:42 +0800

    调整DescHandler的字段

--- deschandler.go
+++ deschandler.go
@@ -2,9 +2,7 @@ package lessgo
 
 // 带描述HandlerFunc
 type DescHandler struct {
-	Handler     HandlerFunc       // 操作
-	Description string            // 本操作的描述
-	Success     string            // 成功后返回的内容描述
-	Failure     string            // 失败后返回的内容描述
-	Param       map[string]string // 参数描述
+	Handler func(Context) error // 操作
+	Desc    string              // 本操作的描述
+	Param   map[string]string   // 参数说明
 }
